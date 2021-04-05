71c5b2ac4a2cb4a7fa4df674962c8d2fe64df417
Alex Christenson <christenson10@gmail.com>
2021-04-05 11:26:40 -0400

    pseudo code, notes

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,107 @@
-// Assignment code here
 
+// Prompt to see how long password is
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+function generatePassword () {
+  var length = prompt ('How long would you like your password to be?');
+  var password = '';
+}
+//If statement between 8 - 128
+  if (length < 8 || length > 128) {
+    window.alert('Please choose within 8 and 128')
+    }
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//Does it have lower case?
+    var lowerCase = window.confirm('Would you like lower case?');
+    if (lowerCase === true) {
+      lower
+      //Add lower case array as one of arrays to password (no else statement)
+    }
+ 
+//Does it have upper case?
+    var upperCase = window.confirm('Would you like lower case?');
+    if (upperCase === true) {
+    
+//Special characters?
+    var specChar = window.confirm('Would you like a special character?');
+    if (specChar === true) {
 
-  passwordText.value = password;
+//Numbers?
+    var pNumber = window.confirm('Would you like a number?');
+    if (pNumber === true) {
 
+//Generate password
+    for (let i = 0; i < length; i++) {
+    const element = array[i];
+
+//Which arrays have been chosen?
+
+//Randomly pick one array
+
+//Randomly pick character from chosen array
+
+//Adding random character to the password
+
+    password = password + GenerateCharacter
+  }
+
+//Return password
+  return password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+// // Assignment code here
+
+// var specialCharacter = ["?", "*", "&", "#", "!", "@"]
+
+// var index = Math.floor(Math.random() * specialCharacter.length);
+
+// var randomSelectedChar = specialCharacter[index];
+
+// console.log("Random Character: ", randomSelectedChar);
+
+// // Get references to the #generate element
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+// Traversy Tutorial
+
+// function getRandomLower() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
+// }
+
+// function getRandomUpper() {
+//   return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+// }
+
+// function getRandomNumber() {
+//   return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
+// }
+
+// function getRandomSymbol() {
+//   const symbols = "!@#$%^&*?><";
+//   return symbols[Math.floor(Math.random() * symbols.length)];
+// }
+
+// console.log(getRandomSymbol());
+
+
