cdc41dba8abebcc3d32906861da77a069ebd0290
Alex Christenson <christenson10@gmail.com>
2021-04-08 17:26:49 -0400

    add q's and write password

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,15 @@
 
+var generateBtn = document.querySelector("#generate");
+
 // Prompt to see how long password is
 
-function generatePassword () {
-  var length = prompt ('How long would you like your password to be?');
-  var password = '';
-}
+function generatePassword() {
+  var length = prompt("How many characters would you like in your password? (Must be between 8 and 128)");
+  var pNumber = confirm("Would you like numbers in your password?");
+  var lowerCase = confirm("Would you like lower in your password??");
+  var upperCase = confirm("Would you like upper case letters in your password??");
+  var specChar = confirm("Would you like special characters in your password??");
+
 //If statement between 8 - 128
   if (length < 8 || length > 128) {
     window.alert('Please choose within 8 and 128')
@@ -47,6 +52,18 @@ function generatePassword () {
 //Return password
   return password;
 }
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 
