5402e9ec170224b1d84b10466b4f3c987aaa18e6
Alex Christenson <christenson10@gmail.com>
2021-04-08 22:11:30 -0400

    cleaned code, added new variables and correct for loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,58 +1,5 @@
-
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Prompt to see how long password is
-
-function generatePassword() {
-  var length = prompt("How many characters would you like in your password? (Must be between 8 and 128)");
-  var pNumber = confirm("Would you like numbers in your password?");
-  var lowerCase = confirm("Would you like lower in your password??");
-  var upperCase = confirm("Would you like upper case letters in your password??");
-  var specChar = confirm("Would you like special characters in your password??");
-
-//If statement between 8 - 128
-  if (length < 8 || length > 128) {
-    window.alert('Please choose within 8 and 128')
-    }
-
-//Does it have lower case?
-    var lowerCase = window.confirm('Would you like lower case?');
-    if (lowerCase === true) {
-      lower
-      //Add lower case array as one of arrays to password (no else statement)
-    }
- 
-//Does it have upper case?
-    var upperCase = window.confirm('Would you like lower case?');
-    if (upperCase === true) {
-    
-//Special characters?
-    var specChar = window.confirm('Would you like a special character?');
-    if (specChar === true) {
-
-//Numbers?
-    var pNumber = window.confirm('Would you like a number?');
-    if (pNumber === true) {
-
-//Generate password
-    for (let i = 0; i < length; i++) {
-    const element = array[i];
-
-//Which arrays have been chosen?
-
-//Randomly pick one array
-
-//Randomly pick character from chosen array
-
-//Adding random character to the password
-
-    password = password + GenerateCharacter
-  }
-
-//Return password
-  return password;
-}
-
 
 // Write password to the #password input
 function writePassword() {
@@ -62,14 +9,56 @@ function writePassword() {
   passwordText.value = password;
 }
 
+//Variables
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var characters = "~!@#$%^&*()_+{}|<>?/[]";
+
+//If statement between 8 - 128
+if (passwordLength < 8 || passwordLength > 128) {
+  alert("Please choose a number between 8 and 128");
+  generatePassword();
+}
+
+// //Does it have lower case?
+// var lowerCase = window.confirm("Would you like lower case?");
+// if (lowerCase === true) {
+//   lower;
+
+// //Add lower case array as one of arrays to password (no else statement)
+// }
+
+// //Does it have upper case?
+// var upperCase = window.confirm("Would you like lower case?");
+// if (upperCase === true)
+
+// //Special characters?
+//   var specChar = window.confirm("Would you like a special character?");
+// if (specChar === true)
+
+// //Numbers?
+//   var pNumber = window.confirm("Would you like a number?");
+// if (pNumber === true)
+
+
+
+// The for loop will begin ramdon selection of variables
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * emptyPassword.length);
+    var randomizedCharacter = emptyPassword[randomIndex];
+    password += randomizedCharacter;
+  }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 
-// // Assignment code here
 
-// var specialCharacter = ["?", "*", "&", "#", "!", "@"]
+
+
+
 
 // var index = Math.floor(Math.random() * specialCharacter.length);
 
@@ -91,34 +80,3 @@ generateBtn.addEventListener("click", writePassword);
 
 // // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-// Traversy Tutorial
-
-// function getRandomLower() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26 + 97));
-// }
-
-// function getRandomUpper() {
-//   return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
-// }
-
-// function getRandomNumber() {
-//   return String.fromCharCode(Math.floor(Math.random() * 10 + 48));
-// }
-
-// function getRandomSymbol() {
-//   const symbols = "!@#$%^&*?><";
-//   return symbols[Math.floor(Math.random() * symbols.length)];
-// }
-
-// console.log(getRandomSymbol());
-
-
