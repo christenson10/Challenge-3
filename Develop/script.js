a5bfeb039012d299a6a7f5a3655868beaac42f56
Alex Christenson <christenson10@gmail.com>
2021-04-08 23:40:34 -0400

    edited and added perameters

--- Develop/script.js
+++ Develop/script.js
@@ -10,73 +10,62 @@ function writePassword() {
 }
 
 //Variables
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var numbers = "0123456789";
-var characters = "~!@#$%^&*()_+{}|<>?/[]";
+var upCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowCase = "abcdefghijklmnopqrstuvwxyz";
+var number = "0123456789";
+var specChar = "~!@#$%^&*()_+{}|<>?/[]";
+var emptyPassword = '';
 
-//If statement between 8 - 128
-if (passwordLength < 8 || passwordLength > 128) {
-  alert("Please choose a number between 8 and 128");
-  generatePassword();
+//Prompt user to make choices
+function charChoice () {
+  var upCaseChoice = confirm("Would you like an upper case letter in your password?");
+  var lowCaseChoice = confirm("Would you like a lower case letter in your password?");
+  var numberChoice = confirm("Would you like a number in your password?");
+  var specCharChoice = confirm("Would you like a special character in your password?");
+
+  //If statements to string together user choices, adds tt
+    if (upCaseChoice){
+      emptyPassword += upCaseChoice
+    }
+    if (lowCaseChoice){
+      emptyPassword += lowCaseChoice
+    }
+    if (numberChoice){
+      emptyPassword += numberChoice
+    }
+    if (specCharChoice){
+      emptyPassword += specCharChoice
+    }
 }
 
-// //Does it have lower case?
-// var lowerCase = window.confirm("Would you like lower case?");
-// if (lowerCase === true) {
-//   lower;
+//Generate password function
+function generatePassword(){
 
-// //Add lower case array as one of arrays to password (no else statement)
-// }
+//Determine password length when prompted
+  var passwordLength = prompt("How long would you like your password to be? Choose between 8 and 128 character.");  //If statement to qualify between 8 - 128
+  
+  //If chosen outside of the parameters
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please choose a number between 8 and 128");
+    generatePassword();
+  }
 
-// //Does it have upper case?
-// var upperCase = window.confirm("Would you like lower case?");
-// if (upperCase === true)
+  //Calls the function
+  charChoice();
 
-// //Special characters?
-//   var specChar = window.confirm("Would you like a special character?");
-// if (specChar === true)
-
-// //Numbers?
-//   var pNumber = window.confirm("Would you like a number?");
-// if (pNumber === true)
-
-
-
-// The for loop will begin ramdon selection of variables
+  //Creates string variable for final password
+  var password = "";
+  
+  //The for loop will begin selecting random variables
   for (var i = 0; i < passwordLength; i++) {
     var randomIndex = Math.floor(Math.random() * emptyPassword.length);
     var randomizedCharacter = emptyPassword[randomIndex];
     password += randomizedCharacter;
   }
 
+  //Returns password
+  return password;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-// var index = Math.floor(Math.random() * specialCharacter.length);
-
-// var randomSelectedChar = specialCharacter[index];
-
-// console.log("Random Character: ", randomSelectedChar);
-
-// // Get references to the #generate element
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
