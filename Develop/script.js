38b5c249763c44eae20dee29f1ceb45e253be0b9
Alex Christenson <christenson10@gmail.com>
2021-04-11 13:59:26 -0400

    final update 2/git issues

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
-// Assignment Code
+//Assignment code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+//Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -18,18 +18,12 @@ var emptyPassword = "";
 
 //Prompt user to make choices
 function charChoice() {
-  var upCaseChoice = confirm(
-    "Would you like an upper case letter in your password?"
-  );
-  var lowCaseChoice = confirm(
-    "Would you like a lower case letter in your password?"
-  );
+  var upCaseChoice = confirm("Would you like an upper case letter in your password?");
+  var lowCaseChoice = confirm("Would you like a lower case letter in your password?");
   var numberChoice = confirm("Would you like a number in your password?");
-  var specCharChoice = confirm(
-    "Would you like a special character in your password?"
-  );
+  var specCharChoice = confirm("Would you like a special character in your password?");
 
-  //If statements to string together user choices. Addition assignment used to determine what is assigned to
+  //If statements to string together user choices. If true, then chosen variable added to charChoice.
   if (upCaseChoice === true) {
     emptyPassword = emptyPassword + upCase;
   }
@@ -51,24 +45,26 @@ function generatePassword() {
     "How long would you like your password to be? Choose between 8 and 128 character."
   );
 
-  //Conditional statement to determine password length
+  //Conditional statement to determine password length and prompt when not chosen within range
   if (passwordLength < 8 || passwordLength > 128) {
     alert("Please choose a number between 8 and 128");
     generatePassword();
   }
 
-  //Calls the function
+  //Calls the function -- isolate within this function
   charChoice();
 
-  //Creates string variable for final password
+  //Creates string variable to put final password
   var password = "";
 
   //The for loop will begin selecting random variables based on password length.
   for (var i = 0; i < passwordLength; i++) {
-    //Randomizes chosen characters
+
+    //Randomizes character choice and then character based on chosen password length.
     var randomIndex = Math.floor(Math.random() * emptyPassword.length);
     var randomizedCharacter = emptyPassword[randomIndex];
 
+    //Adds random characters to password
     password = password + randomizedCharacter;
   }
 
